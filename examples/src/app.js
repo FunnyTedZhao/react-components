bf655c3e9476e9055114241ddc79f7a84f011407
ZhaoLong <zhaolong@cennavi.com.cn>
2020-04-27 11:37:30 +0800

    chore: 组件库基本框架搭建

---
+++ examples/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'react-components/style/component.css';// eslint-disable-line
+import Block from 'react-components/src'; // eslint-disable-line
+
+ReactDOM.render(
+  <div className="component-template">
+    <Block />
+  </div>,
+  document.getElementById('example'),
+);
