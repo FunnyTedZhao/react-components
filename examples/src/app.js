9c1ded77399f2bcdae82df9d7876e11dc1eef043
FunnyTedZhao <15201321241@163.com>
2020-05-11 14:57:27 +0800

    refactor: 调整示例展示

--- examples/src/app.js
+++ examples/src/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'react-components/style/component.css';// eslint-disable-line
-import Block from 'react-components/src'; // eslint-disable-line
+import {Block, AreaSelector} from 'react-components/src'; // eslint-disable-line
 
 ReactDOM.render(
-  <div className="component-template">
+  <div className="component-template" style={{ padding: '20px' }}>
     <Block />
+    <AreaSelector value={['650000', '469023']} />
   </div>,
   document.getElementById('example'),
 );
